99d52253310a671bd576edf4b13d784ab916b958
luke <lukaspie00@gmail.com>
2020-01-13 14:00:01 +0100

    Implemented GraphImpl and WeightedGraphImpl

--- src/GraphImpl.java
+++ src/GraphImpl.java
@@ -1,25 +1,57 @@
+import java.util.ArrayList;
+
 public class GraphImpl implements Graph {
-  public GraphImpl(int[][] a) {
+  private int[][] a;
   
+  public GraphImpl(int[][] adjacencyMatrix) {
+	a = adjacencyMatrix;
   }
   
   @Override
   public int size() {
-	return 0;
+	return a.length;
   }
   
   @Override
   public int deg(int v) {
-	return 0;
+	return a[v].length;
   }
   
   @Override
   public int succ(int v, int i) {
-	return 0;
+	return a[v][i];
   }
   
   @Override
   public Graph transpose() {
-	return null;
+	//TODO laufzeit is noch übelster Bullshit. Geht bestimmt noch besser, aber fürs erste klappts :)
+	int[][] aTransposed = new int[a.length][];
+	ArrayList<Integer>[] temp = new ArrayList[size()];
+	
+	for (int i = 0; i < size(); i++) {
+	  for (int j = 0; j < a[i].length; j++) {
+		int ind = a[i][j];
+		if (temp[ind] == null) temp[ind] = new ArrayList<>();
+		temp[ind].add(i);
+	  }
+	}
+	
+	for (int i = 0; i < size(); i++) {
+	  aTransposed[i] = new int[temp[i].size()];
+	  for (int j = 0; j < temp[i].size(); j++) {
+		aTransposed[i][j] = temp[i].get(j);
+	  }
+	}
+	
+	return new GraphImpl(aTransposed);
+  }
+  
+  private int countNum(int n) {
+	int c = 0;
+	for (int i = 0; i < a.length; i++) {
+	  for (int j = 0; j < a[i].length; j++) if (a[i][j] == n) c++;
+	}
+	
+	return c;
   }
 }
--- src/WeightedGraphImpl.java
+++ src/WeightedGraphImpl.java
@@ -1,29 +1,55 @@
+import java.util.ArrayList;
+
 public class WeightedGraphImpl implements WeightedGraph {
-  public WeightedGraphImpl(int[][] a, double[][] w) {
+  private int[][] a;
+  private double[][] w;
+  
+  public WeightedGraphImpl(int[][] adjacencyMatrix, double[][] weights) {
+	a = adjacencyMatrix;
+	w = weights;
   }
   
   @Override
   public int size() {
-	return 0;
+	return a.length;
   }
   
   @Override
   public int deg(int v) {
-	return 0;
+	return a[v].length;
   }
   
   @Override
   public int succ(int v, int i) {
-	return 0;
+	return a[v][i];
   }
   
   @Override
   public Graph transpose() {
-	return null;
+	//TODO laufzeit is noch übelster Bullshit. Geht bestimmt noch besser, aber fürs erste klappts :)
+	int[][] aTransposed = new int[a.length][];
+	ArrayList<Integer>[] temp = new ArrayList[size()];
+	
+	for (int i = 0; i < size(); i++) {
+	  for (int j = 0; j < a[i].length; j++) {
+		int ind = a[i][j];
+		if (temp[ind] == null) temp[ind] = new ArrayList<>();
+		temp[ind].add(i);
+	  }
+	}
+	
+	for (int i = 0; i < size(); i++) {
+	  aTransposed[i] = new int[temp[i].size()];
+	  for (int j = 0; j < temp[i].size(); j++) {
+		aTransposed[i][j] = temp[i].get(j);
+	  }
+	}
+	
+	return new GraphImpl(aTransposed);
   }
   
   @Override
   public double weight(int v, int i) {
-	return 0;
+	return w[v][i];
   }
 }
