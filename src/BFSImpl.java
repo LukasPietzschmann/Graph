26571160a4dd50f78b21d7362fd82c90a0b74b34
Remoo-t <joseph.rieger@yahoo.de>
2020-01-14 15:02:05 +0100

    implemented search

--- src/BFSImpl.java
+++ src/BFSImpl.java
@@ -1,7 +1,39 @@
+import java.util.*;
+
 public class BFSImpl implements BFS {
+   private int[] dist;
+   private int[] pred;
+    private Queue<Integer> q = new LinkedList<>();
+
   @Override
   public void search(Graph g, int s) {
-	return;
+	dist = new int[g.size()];
+	pred = new int[g.size()];
+	int u;
+
+	for(int i = 0; i < g.size(); i++)
+    {
+        this.dist[i] = INF;
+        this.pred[i] = INF;
+    }
+
+	this.dist[s] = 0;
+
+	this.q.add(s);
+
+	while(!q.isEmpty())
+    {
+        u = this.q.remove();
+        for(int v = 0; v < g.deg(u); v++)
+        {
+            if(dist(v) == INF)
+                this.dist[v] = dist(u) + 1;
+                this.pred[v] = u;
+                q.add(v);
+        }
+
+    }
+
   }
   
   @Override
