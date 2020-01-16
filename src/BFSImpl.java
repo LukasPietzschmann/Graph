7e3604d742d02a65770ae87988140f15697fa3c3
luke <lukaspie00@gmail.com>
2020-01-16 11:00:12 +0100

    Fixed BFSImpl

--- src/BFSImpl.java
+++ src/BFSImpl.java
@@ -1,50 +1,48 @@
 import java.util.*;
 
 public class BFSImpl implements BFS {
-   private int[] dist;
-   private int[] pred;
-    private Queue<Integer> q = new LinkedList<>();
-
+  private int[] dist;
+  private int[] pred;
+  private Queue<Integer> q = new LinkedList<>();
+  
   @Override
   public void search(Graph g, int s) {
 	dist = new int[g.size()];
 	pred = new int[g.size()];
 	int u;
 	int v;
-
-	for(int i = 0; i < g.size(); i++)
-    {
-        this.dist[i] = INF;
-        this.pred[i] = INF;
-    }
-
-	this.dist[s] = 0;
-
-	this.q.add(s);
-
-	while(!q.isEmpty())
-    {
-        u = this.q.remove();
-        for(int i = 0; i < g.deg(u); i++)
-        {
-            v = g.succ(u, i);
-            if(dist(v) == INF)
-                this.dist[v] = dist(u) + 1;
-                this.pred[v] = u;
-                q.add(v);
-        }
-
-    }
-
+	
+	for (int i = 0; i < g.size(); i++) {
+	  dist[i] = INF;
+	  pred[i] = INF;
+	}
+	
+	dist[s] = 0;
+	
+	q.add(s);
+	
+	while (!q.isEmpty()) {
+	  u = q.poll();
+	  
+	  for (int i = 0; i < g.deg(u); i++) {
+		v = g.succ(u, i);
+		
+		if (dist(v) == INF) {
+		  dist[v] = dist(u) + 1;
+		  pred[v] = u;
+		  q.add(v);
+		}
+	  }
+	}
   }
   
   @Override
   public int dist(int v) {
-	return this.dist[v];
+	return dist[v];
   }
   
   @Override
   public int pred(int v) {
-	return this.pred[v];
+	return pred[v];
   }
 }
